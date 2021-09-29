62adc3106253679b51259c6245cc317545feb3f1
giteauser <user@gitea.ao>
2021-09-29 11:02:57 +0100

    merge from github

--- podman-init.sh
+++ podman-init.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 # create common volumes
-podman volume create devopsc 
-podman volume create devopci 
+#podman volume create devopsc 
+#'podman volume create devopci 
 
 # create common network
-podman network create br192-168 \
+podman network rm podman
+podman network create podman \
 	--driver bridge \
 	--subnet 192.167.55.0/24 \
-	--ip-range 192.167.55.128/25 \
+	--ip-range 192.167.55.129/25 \
 	--gateway 192.167.55.1
