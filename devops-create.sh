975573cae910b8b8ee1075f58cf6f020a79392a7
giteauser <user@gitea.ao>
2021-10-01 17:11:43 +0100

    pod create start script

--- devops-create.sh
+++ devops-create.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 # define command line options
-OPS="--pod devops"
-SCM="--name giteascm -v scm_data:/var/gitrepo"
-CID="--name jenkinscid -v cid_data:/var/www/jenkins"
+OPS="-n devops -p 8080:8080 -p 8081:8081"
+SCM="--pod devops --name giteascm -v scm_data:/var/gitrepo"
+CID="--pod devops --name jenkinscid -v cid_data:/var/www/jenkins"
 
 # create a pod named devops if it doesn't exist
-podman pod exists devops || podman pod create -n devops -p "8080:8080" -p "8081:8081"
+podman pod exists devops || podman pod create $OPS
 
 # create devops containers if they don't exist
-podman pod inspect devops|grep "giteascm" || podman create $OPS $SCM ao/giteascm
-podman pod inspect devops|grep "jenkinscid" || podman create $OPS $CID ao/jenkinscid
+podman pod inspect devops|grep "giteascm" || podman create $SCM ao/giteascm
+podman pod inspect devops|grep "jenkinscid" || podman create $CID ao/jenkinscid
 
