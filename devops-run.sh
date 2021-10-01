ad920134ca30cd53232c63c7c8c5166ef3f0f49c
giteauser <user@gitea.ao>
2021-10-01 12:35:02 +0100

    rename container -> docker

--- devops-run.sh
+++ devops-run.sh
@@ -2,6 +2,9 @@
 
 # define command line options
 OPS=" -d --replace --rm "
-GITEA="--name giterascm -v scm_data:/var/gitrepo -p 8081:8081 "
+GITEA="--name giteascm -v scm_data:/var/gitrepo -h giteascm --expose 8081 -p 8081:8081 "
+JENKINS="--name jenkinscid -v cid_data:/var/www/jenkins -h jenkinscid -p 8080:8080 "
 
-podman run $OPS $GITEA ao/giteascm 
+podman run $OPS $GITEA ao/giteascm && \
+podman run $OPS $JENKINS ao/jenkinscid
+
--- mongocli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CMD="mongo --host my.mongo -u mongodb -p m-n.g+db"
-
-docker run -it --rm --network br192 \
-	--hostname my.mongocli --ip 192.168.2.11 \
-	mongo:4 $CMD
-
