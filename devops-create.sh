2c911ce7f820f9dd54f382727deb316a45e8bf16
giteauser <user@gitea.ao>
2021-10-15 10:32:33 +0100

    seperate the app volume

--- devops-create.sh
+++ devops-create.sh
@@ -5,7 +5,8 @@ POD="-n devops -p 8080-8081:8080-8081"
 
 # containers
 SCM="--pod devops --replace --name giteascm -v scm_data:/var/gitrepo"
-CID="--pod devops --replace --name jenkinscid -v cid_data:/var/www/jenkins -v stock_apps:/apps"
+CID="--pod devops --replace --name jenkinscid -v cid_data:/var/www/jenkins"
+VOL="-v stock_apps:/apps"
 
 # create a pod named devops if it doesn't exist
 podman pod exists devops || podman pod create $POD
@@ -13,4 +14,4 @@ podman pod exists devops || podman pod create $POD
 #podman pod inspect devops|grep "giteascm" || 
 podman create $SCM ao/giteascm
 #podman pod inspect devops|grep "jenkinscid" || 
-podman create $CID ao/jenkinscid
+podman create $CID $VOL ao/jenkinscid
