720b07b75cef30699bca662c09fe64d05d28b91e
Ahmet OZKESEK <aozkesek@gmail.com>
2022-06-12 14:18:02 +0100

    allow tcp connection

--- postgres-build/pg-entry.sh
+++ postgres-build/pg-entry.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+HBA=$PGDATA/pg_hba.conf
+
 chown -R postgres "$PGDATA"
 if [ -z "$(ls -A "$PGDATA")" ]; then
     su postgres -c 'initdb'
-    echo "host all all $HOSTNAME trust" >> $PGDATA/pg_hba.conf
+    cp $HBA $HBA.org
 fi
+cp $HBA.org $HBA
+echo "host all all $HOSTNAME trust" >> $HBA
 su postgres -c 'postgres -h $HOSTNAME -s -i -p 5432 -d 4'
